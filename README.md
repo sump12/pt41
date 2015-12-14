d95837721dd796a2a4bdd2402bb9ed00be6c2d18
test@test.com <test@test.com>
2015-12-14 21:10:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Steampunk (PNK)
+
+PoS Only
+Supply: 40 Millions
+12 hours minimum stake aged
+Unlimites max stake aged
+PoS Yearly interest: 8%
+
---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x0000056ba6458ab1889be67403384c02bac10eceb4eb6d8f2289771c977834e1");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
